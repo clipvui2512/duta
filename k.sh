dc901daa4c0c1c99655c17ea2f9baf003c05c221
clipvui2512 <clipvui2512@gmail.com>
2021-10-23 23:42:42 +0700

    Update k.sh
    s

--- k.sh
+++ k.sh
@@ -1,3 +1,3 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-static-x64.tar.gz && tar -xvf xmrig-6.15.2-linux-static-x64.tar.gz && cd xmrig-6.15.2
-myworker=$(date +'%d%m_%H%M%S'
+myworker=$(date +'%d%m_%H%M%S')
 ./xmrig -o pool.minexmr.com:4444 -u 4Ahd3Nv3DxMNp5wv1SxoT2NL1BjowyfoxLXnv6mfpTNFb5uYcicH9QZZzVTke2X6it1Qicv8VALuN46xbsdMZhWaVg6KuVf.$myworker
